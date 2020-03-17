7eca5b83e2e725d72fefa510ebbcc8e3a0c2f993
David Andersson <david.andersson@zocom.se>
2020-03-17 15:48:29 +0100

    Final version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,35 @@
 <template>
 	<div class="hello">
 		<h1>Welcome to Vue intro</h1>
-		<div v-bind:class="myClass">
+		<div v-bind:class="myClass" v-bind:style="{ 'background-color': myStyle}">
 			The value is {{ count }}
 		</div>
-		<button @click="myClass = 'unicorn'">Set color to BLUE</button>
-		<button @click="myClass = 'racoon'">Set color to MAGENTA</button>
+
+		<div class="buttons">
+			<input type="text" @keyup="handleKeyUp" />
+			<button @click="handleClickSetColor">Set color?</button>
+			<!-- {{ inputColor }} -->
+			<br />
+
+			<button @click="myClass = 'unicorn'">Set color to BLUE</button>
+			<button @click="myClass = 'racoon'">Set color to MAGENTA</button>
+			<button @click="myClass = 'basilisk'">Set basilisk</button>
+		</div>
+		{{ myClass }}
+
+		<div v-if="myClass == 'racoon'">
+			Only visible if we have a racoon
+		</div>
+		<div v-else-if="myClass == 'unicorn'">
+			Only visible if we have a unicorn
+		</div>
+		<div v-else>
+			Something else?
+		</div>
+		<div v-show="myClass == 'racoon'">
+			Also only visible if we have a racoon
+		</div>
+
 	</div>
 </template>
 
@@ -17,8 +41,19 @@ export default {
 	},
 	data: () => ({
 		count: 1,
-		myClass: 'unicorn'
-	})
+		myClass: 'unicorn',
+		myStyle: '',
+		inputColor: ''
+	}),
+	methods: {
+		handleKeyUp(event) {
+			console.log('handleKeyUp event=', event);
+			this.inputColor = event.target.value;
+		},
+		handleClickSetColor() {
+			this.myStyle = this.inputColor;
+		}
+	}
 }
 </script>
 
@@ -27,11 +62,27 @@ export default {
 /* .myClass {
 	background-color: green;
 } */
+.em-small {
+	height: 1em;
+}
+.em-large {
+	height: 2em;
+}
+.em-small, .em-large {
+	border: 1px solid black;
+	width: 3.5em;
+}
+.buttons {
+	margin-top: 2rem;
+}
 .unicorn {
 	background-color: lightblue;
 }
 .racoon {
 	background-color: magenta;
+}
+.basilisk {
+	background-color: gray;
 }
 h3 {
 	margin: 40px 0 0;
